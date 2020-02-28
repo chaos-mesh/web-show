d5224605db5a59a7de8caefa426f924fb96c859c
cwen0 <cwenyin0@gmail.com>
2020-02-28 15:38:14 +0800

    ignore a little error
    Signed-off-by: cwen0 <cwenyin0@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ kubectl apply -f deploy/
 
 while [[ $(kubectl get pods -l app=web-show -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod running" && sleep 1; done
 
-kill $(lsof -t -i:8081)
+kill $(lsof -t -i:8081) 2>&1 >/dev/null | True
 
 nohup kubectl port-forward svc/web-show 8081:8081 2>&1 &
